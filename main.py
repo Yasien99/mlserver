fac6a3b6fb584e463050044fa9c4779c79f6c94a
Yasien Essam <yasien.essam99@gmail.com>
2024-07-13 15:24:10 +0300

    add logging and preform crop for approval number not on the full images

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from torchvision import models, transforms
+from inspect import iscoroutinefunction
+from functools import wraps, partial
 from pydantic import BaseModel
 from fastapi import FastAPI
 from io import BytesIO
@@ -6,12 +8,54 @@ import torch.nn as nn
 from PIL import Image
 import numpy as np
 import requests
+import logging 
 import uvicorn
 import easyocr
 import torch
+import time
 import cv2
 import re 
 app = FastAPI()
+
+
+# Configure logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
+def log_execution_time(func=None, *, is_async=False):
+    if func is None:
+        return partial(log_execution_time, is_async=is_async)
+
+    if iscoroutinefunction(func) or is_async:
+        @wraps(func)
+        async def async_wrapper(*args, **kwargs):
+            start_time = time.time()
+            logger.debug(f"Started {func.__name__} with args: {args} and kwargs: {kwargs}")
+            result = await func(*args, **kwargs)
+            end_time = time.time()
+            execution_time = end_time - start_time
+            logger.debug(f"Finished {func.__name__} in {execution_time:.4f} seconds")
+            return result
+        return async_wrapper
+    else:
+        @wraps(func)
+        def sync_wrapper(*args, **kwargs):
+            start_time = time.time()
+            logger.debug(f"Started {func.__name__} with args: {args} and kwargs: {kwargs}")
+            result = func(*args, **kwargs)
+            end_time = time.time()
+            execution_time = end_time - start_time
+            logger.debug(f"Finished {func.__name__} in {execution_time:.4f} seconds")
+            return result
+        return sync_wrapper
 
 class ImageURLs(BaseModel):
     urls: list[str]
@@ -45,11 +89,13 @@ transform = transforms.Compose([
     transforms.ToTensor(),
 ])
 
+@log_execution_time
 def preprocess_image(image: Image.Image):
     image = transform(image)
     image = image.unsqueeze(0)
     return image
 
+@log_execution_time
 def perform_ocr_on_claim(image: Image.Image):
     # Convert PIL image to OpenCV format
     image = np.array(image)
@@ -71,13 +117,21 @@ def perform_ocr_on_claim(image: Image.Image):
 
     return extracted_numbers
 
-def perform_ocr_on_full_approval(image: Image.Image):
+@log_execution_time
+def perform_ocr_on_crop_approval_number(image: Image.Image):
     # Convert PIL image to OpenCV format
     image = np.array(image)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
+    # Resize the image to 1000x1000 pixels
+    resized_image = cv2.resize(image, (1000, 1000))
+
+    # Select the ROI manually (adjust coordinates as needed)
+    x, y, w, h = 0, 100, 1000, 250
+    roi = resized_image[y:y+h, x:x+w]
+
     # Perform OCR on the cropped image
-    results = reader.readtext(image)
+    results = reader.readtext(roi)
 
     # Extract the numbers matching the regular expression
     number_pattern = re.compile(r'^\d{6,}$')
@@ -85,7 +139,8 @@ def perform_ocr_on_full_approval(image: Image.Image):
 
     return extracted_numbers
 
-def perform_ocr_on_crop_approval(image: Image.Image):
+@log_execution_time
+def perform_ocr_on_crop_approval_card_number(image: Image.Image):
     # Convert PIL image to OpenCV format
     image = np.array(image)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
@@ -94,7 +149,7 @@ def perform_ocr_on_crop_approval(image: Image.Image):
     resized_image = cv2.resize(image, (1000, 1000))
 
     # Select the ROI manually (adjust coordinates as needed)
-    x, y, w, h = 0, 280, 1000, 100
+    x, y, w, h = 500, 280, 1000, 150
     roi = resized_image[y:y+h, x:x+w]
 
     # Perform OCR on the cropped image
@@ -111,6 +166,7 @@ def read_root():
     return {"Hello": "World"}
 
 @app.post("/predict/")
+@log_execution_time
 async def predict(images: ImageURLs):
     results = []
     for url in images.urls:
@@ -138,10 +194,10 @@ async def predict(images: ImageURLs):
 
                 elif image_class == "approval":
                     original_image = Image.open(BytesIO(response.content)).convert("RGB")
-                    extracted_numbers = perform_ocr_on_crop_approval(original_image)
+                    extracted_numbers = perform_ocr_on_crop_approval_card_number(original_image)
                     card_number = extracted_numbers[0] if extracted_numbers else ""
 
-                    extracted_numbers = perform_ocr_on_full_approval(original_image)
+                    extracted_numbers = perform_ocr_on_crop_approval_number(original_image)
                     approval_number = extracted_numbers[0] if extracted_numbers else ""
 
                     results.append({
